4176583afb859de29381739af0f649737eb7ac84
AirChen <renzhichen2012@163.com>
2021-03-10 22:40:44 +0800

    Add bing background image

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ import requests
 from jinja2 import FileSystemLoader, Environment
 import json
 import copy
+from urllib import parse, request
 
 def get_rss_info(feed_url):
     result = {"result": []}
     # 如何请求出错,则重新请求,最多五次
-    for i in range(1):
+    for i in range(5):
         try:
             headers = {
                 # 设置用户代理头(为狼披上羊皮)
@@ -109,8 +110,8 @@ def replace_readme():
         for line in edit_readme_lines:
             if re.match(r'.*\[订阅地址\]\(.*\).*\{\{latest_content\}\}.*', line) != None:
                 new_line, entity_list = process_line(line)                
-                session['list'] = session['list'] + entity_list
-                g_rss_num = g_rss_num + len(entity_list)
+                session['list'] += entity_list[:5]
+                g_rss_num += 1
             elif re.match(r'.*\<h2 id\=\".*\"\>.*\<\/h2\>.*', line) != None:
                 if g_rss_num != 0:
                     session_list.append(session)
@@ -146,13 +147,32 @@ def get_email_list():
             email_list.append(task["email"])
     return email_list
 
+def get_bing_img_url():
+    BING_API = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160"
+    BING_URL = "https://cn.bing.com"
+    
+    header_info = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
+    req = request.Request(url=BING_API, headers=header_info)
+    res = request.urlopen(req)
+    res = res.read()
+
+    json_obj = json.loads(res) 
+    # res_data = str(res.decode(encoding='utf-8')) 转码utf-8后
+
+    url_params = json_obj['images'][0]['url']
+    url_img = BING_URL + url_params.split('&')[0]
+
+    return url_img
+
 def main():
     session_list = replace_readme()
+    bing_img = get_bing_img_url()
 
     env = Environment(loader=FileSystemLoader('./templates'))
     template = env.get_template('basic.html')
     cout_html = template.render(web_title="RSS订阅", 
                 rss_num=g_rss_num, ga_rss_datetime=g_rss_datetime,
+                back_img=bing_img,
                 session_list=session_list)
 
     with open(os.path.join(os.getcwd(),"render.html"),'w') as load_f:
